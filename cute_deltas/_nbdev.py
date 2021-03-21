c269c67db004eda4f148de50291b21990d7d1339
Nathan Cooper <nacooper01@email.wm.edu>
2021-03-21 17:39:14 +0000

    update lib and docs

--- cute_deltas/_nbdev.py
+++ cute_deltas/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"levenshtein_distance": "00_algos.ipynb"}
 
-modules = []
+modules = ["algos.py"]
 
 doc_url = "https://ncoop57.github.io/cute_deltas/"
 
